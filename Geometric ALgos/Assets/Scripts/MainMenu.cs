44a15bc1a0522a97b48aa1437136c4f929ddf2c9
merkelmauer <markopetkovic1999@gmail.com>
2021-11-28 15:56:58 +0100

    Created game files and added main menu

---
+++ Geometric ALgos/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject mainMenu;
+    public GameObject howToMenu;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void howToGame()
+    {
+        mainMenu.SetActive(false);
+        howToMenu.SetActive(true);
+    }
+
+    public void ReturnToMenu()
+    {
+        mainMenu.SetActive(true);
+        howToMenu.SetActive(false);
+    }
+
+}
